84cd9593bc6d9a94c9618981e4c4c27f71883b7d
RyanKJ <ryankj42@gmail.com>
2018-10-01 15:10:45 -0500

    Add get_video_model() function to __init__.py
    Created functions that identically mimic the vanilla wagtail images get_image_model() function. This function allows one to set a custom video model in the settings.py file under the WAGTAILVIDEOS_VIDEO_MODEL variable. get_video_model() retrieves a custom defined video model or the default wagtailvideo.Video model.

--- wagtailvideos/__init__.py
+++ wagtailvideos/__init__.py
@@ -1,1 +1,33 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
 default_app_config = 'wagtailvideos.apps.WagtailVideosApp'
+
+
+def get_video_model_string():
+    """
+    Get the dotted ``app.Model`` name for the video model as a string.
+    Useful for developers making Wagtail plugins that need to refer to the
+    video model, such as in foreign keys, but the model itself is not required.
+    """
+    return getattr(settings, 'WAGTAILVIDEOS_VIDEO_MODEL', 'wagtailvideos.Video')
+
+
+def get_video_model():
+    """
+    Get the video model from the ``WAGTAILVIDEOSVIDEO_MODEL`` setting.
+    Useful for developers making Wagtail plugins that need the video model.
+    Defaults to the standard :class:`~wagtailvideo.models.Video` model
+    if no custom model is defined.
+    """
+    from django.apps import apps
+    model_string = get_image_model_string()
+    try:
+        return apps.get_model(model_string)
+    except ValueError:
+        raise ImproperlyConfigured("WAGTAILVIDEOS_VIDEO_MODEL must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured(
+            "WAGTAILVIDEOS_VIDEO_MODEL refers to model '%s' that has not been installed" % model_string
+        )
