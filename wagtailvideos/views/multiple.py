c784a5c0272cfb5636f947fc9ffad6c069dd58a8
RyanKJ <ryankj42@gmail.com>
2018-10-01 16:57:52 -0500

    Add get_video_model() functionality to multiple.py

--- wagtailvideos/views/multiple.py
+++ wagtailvideos/views/multiple.py
@@ -8,7 +8,7 @@ from wagtail.admin.utils import PermissionPolicyChecker
 from wagtail.search.backends import get_search_backends
 
 from wagtailvideos.forms import get_video_form
-from wagtailvideos.models import Video
+from wagtailvideos import get_video_model
 from wagtailvideos.permissions import permission_policy
 
 permission_checker = PermissionPolicyChecker(permission_policy)
@@ -30,6 +30,7 @@ def get_video_edit_form(VideoModel):
 
 @vary_on_headers('X-Requested-With')
 def add(request):
+    Video = get_video_model()
     VideoForm = get_video_form(Video)
 
     collections = permission_policy.collections_user_has_permission_for(request.user, 'add')
@@ -91,6 +92,7 @@ def add(request):
 
 @require_POST
 def edit(request, video_id, callback=None):
+    Video = get_video_model()
     VideoForm = get_video_edit_form(Video)
 
     video = get_object_or_404(Video, id=video_id)
@@ -126,7 +128,7 @@ def edit(request, video_id, callback=None):
 
 @require_POST
 def delete(request, video_id):
-    video = get_object_or_404(Video, id=video_id)
+    video = get_object_or_404(get_video_model(), id=video_id)
 
     if not request.is_ajax():
         return HttpResponseBadRequest("Cannot POST to this view without AJAX")
